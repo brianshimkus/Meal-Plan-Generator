1dfc5a7f968b8396c009c4d7472cfe676815ccf9
Brian Shimkus <brianshimkus1@gmail.com>
2025-04-04 18:35:47 -0500

    Added sign in and sign out to nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import {
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	SignOutButton,
+	useUser,
+} from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function NavBar() {
+	const { isLoaded, isSignedIn, user } = useUser()
+	if (!isLoaded) return <p>Loading...</p>
 	return (
 		<nav>
 			<div>
@@ -10,7 +21,32 @@ export default function NavBar() {
 				</Link>
 			</div>
 
-			<div>Page Links</div>
+			<div>
+				<SignedIn>
+					<Link href='/mealplan'>Meal Plan</Link>
+					{user?.imageUrl ? (
+						<Link href='/profile'>
+							<Image
+								src={user.imageUrl}
+								alt={user.fullName || 'Profile Picture'}
+								width={40}
+								height={40}
+							/>
+						</Link>
+					) : (
+						<div>User</div>
+					)}
+
+					<SignOutButton>
+						<button>Sign Out</button>
+					</SignOutButton>
+				</SignedIn>
+				<SignedOut>
+					<SignInButton>
+						<button>Sign In</button>
+					</SignInButton>
+				</SignedOut>
+			</div>
 		</nav>
 	)
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: '**.clerk.com', // Allow all subdomains of clerk.com
+				port: '',
+				pathname: '/**', // Allow all paths
+			},
+		],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
